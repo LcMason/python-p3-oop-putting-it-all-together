b3cbd3a277dd62b2ecbf023409641687b3a6257c
Lionel Mason <capitalcookhouse@gmail.com>
2024-12-07 14:06:02 -0500

    completes putting-it-all-together lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -2,3 +2,34 @@
 
 class Shoe:
     pass
+
+
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "Old"
+
+    def get_brand(self):
+        return self._brand
+
+    def set_brand(self, brand):
+        self._brand = brand
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print('size must be an integer')
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+
+
+
+    brand = property(get_brand, set_brand)
+    size = property(get_size, set_size)
